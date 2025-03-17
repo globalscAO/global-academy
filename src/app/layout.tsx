1a84c32a249e5f441d57eef2ba5b983be817b970
globalscAO <gservicescorporation@gmail.com>
2025-03-17 10:56:27 +0100

    feat: added new metadatas for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,44 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  variable: "--font-inter-sans",
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Global Academy - Conhecimento que Transforma",
+  title: "Global Academy | Global Services Corporation",
   description:
     "A Global Academy, parte da Global Services Corporation, oferece conhecimento de alto nível para impulsionar carreiras e negócios.",
+  keywords: [
+    "formação angola",
+    "academy angola",
+    "global academy angola",
+    "formação corporativa",
+    "centro de formação em Luanda",
+    "cursos profissionais Luanda",
+    "treinamento empresarial Angola",
+    "capacitação corporativa",
+    "certificação profissional Angola",
+    "workshops empresariais",
+
+    "global services angola",
+    "global services corporation",
+    "multiserviços Angola",
+    "consultoria empresarial Angola",
+    "soluções corporativas Luanda",
+    "desenvolvimento profissional Angola",
+    "assessoria estratégica Angola",
+
+    "networking corporativo",
+    "eventos empresariais Luanda",
+    "fórum de negócios Angola",
+    "parcerias estratégicas Angola",
+    "conferências corporativas",
+
+    "formação para setor petrolífero",
+    "treinamento para bancos Angola",
+    "capacitação para construtoras",
+  ],
   openGraph: {
     type: "website",
     url: "https://academy.globalsc.ao",
@@ -19,7 +49,7 @@ export const metadata: Metadata = {
       "A Global Academy, parte da Global Services Corporation, oferece conhecimento de alto nível para impulsionar carreiras e negócios.",
     images: [
       {
-        url: "/seo.jpg",
+        url: "https://academy.globalsc.ao/seo.jpg",
         width: 1200,
         height: 630,
         alt: "Global Academy - O Conhecimento que Transforma",
@@ -32,7 +62,10 @@ export const metadata: Metadata = {
     title: "Global Academy - Conhecimento que Transforma",
     description:
       "A Global Academy, parte da Global Services Corporation, oferece conhecimento de alto nível para impulsionar carreiras e negócios.",
-    images: ["/seo.jpg"],
+    images: ["https://academy.globalsc.ao/seo.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
   },
 };
 
